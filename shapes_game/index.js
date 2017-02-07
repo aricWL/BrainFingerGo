7aa224e4cee342e12c427e109ce0957f3762f249
aricWL <aricliesenfelt@gmail.com>
2017-02-06 17:06:43 -0800

    Adding Game

---
+++ shapes_game/index.js
@@ -0,0 +1,313 @@
+ var score = 0;
+ //Grab user data from database will be stored here
+ var userArray = [];
+ // Prevent user from seeing shapes in the game before it starts
+ window.addEventListener("load", function() {
+     $('#overlay').show();
+
+
+     //Define Canvas
+     var canvas = document.getElementById("shapes-game"),
+         height = canvas.scrollHeight,
+         width = canvas.scrollWidth,
+         gameOn = false,
+         expectedKey = undefined,
+         ctx = canvas.getContext('2d'),
+         // white triangle = up, red square = down,
+         // red triangle = left, white square = right
+         expectedKeysMap = {
+             white0: 38,
+             red1: 40,
+             red0: 37,
+             white1: 39
+         },
+         timerSpan = document.getElementById("time-remaining"),
+         scoreSpan = document.getElementById("score-val"),
+         seconds = 3,
+         intervalId;
+
+     canvas.width = width;
+     canvas.height = height;
+
+
+
+
+
+     //******************SHAPE FUNCTIONS***********************//
+     var randomSquareCorner = Math.floor(Math.random() * 300 + 50);
+     //SHAPES
+     var squareRed = {
+         corner: [Math.floor(Math.random() * 650 + 50), Math.floor(Math.random() * 650 + 50)],
+         width: 50,
+         height: 50,
+         color: "red",
+         draw: function() {
+             ctx.fillStyle = this.color;
+             ctx.fillRect(this.corner[0], this.corner[1], this.width, this.height);
+         }
+     }
+
+     var squareWhite = {
+         corner: [Math.floor(Math.random() * 650 + 50), Math.floor(Math.random() * 650 + 50)],
+         width: 50,
+         height: 50,
+         color: "white",
+         draw: function() {
+             ctx.fillStyle = this.color;
+             ctx.fillRect(this.corner[0], this.corner[1], this.width, this.height);
+         }
+     }
+
+     function drawSquareRed() {
+         ctx.clearRect(0, 0, canvas.width, canvas.height);
+         squareRed.corner[0];
+         squareRed.corner[1];
+         squareRed.draw();
+     }
+
+     function drawSquareWhite() {
+         ctx.clearRect(0, 0, canvas.width, canvas.height);
+         squareWhite.corner[0];
+         squareWhite.corner[1];
+         squareWhite.draw();
+     }
+
+
+     function drawTriangleWhite() {
+         ctx.clearRect(0, 0, canvas.width, canvas.height);
+         var x = Math.floor(Math.random() * 650 + 50);
+         var y = Math.floor(Math.random() * 650 + 50);
+         ctx.beginPath();
+         ctx.fillStyle = "white";
+         ctx.moveTo(x, y);
+         ctx.lineTo(x + 50, y + 50);
+         ctx.lineTo(x, y + 50);
+         ctx.fill();
+         ctx.closePath();
+     }
+
+     function drawTriangleRed() {
+         ctx.clearRect(0, 0, canvas.width, canvas.height);
+         var x = Math.floor(Math.random() * 650 + 50);
+         var y = Math.floor(Math.random() * 650 + 50);
+         ctx.beginPath();
+         ctx.fillStyle = "red";
+         ctx.moveTo(x, y);
+         ctx.lineTo(x + 50, y + 50);
+         ctx.lineTo(x, y + 50);
+         ctx.fill();
+         ctx.closePath();
+     }
+
+
+
+     //*********************PICK RANDOM SHAPE EVERY 3 SECONDS *****************************//
+     var current = 0;
+
+     function pickRandomShape() {
+         var arrayOfShapes = [drawTriangleRed, drawTriangleWhite, drawSquareRed, drawSquareWhite];
+         var random = arrayOfShapes[Math.floor(Math.random() * 4)];
+
+         if (random === drawTriangleRed) {
+             current = 37;
+
+         } else if (random === drawSquareWhite) {
+             current = 39;
+
+
+         } else if (random === drawTriangleWhite) {
+             current = 38;
+
+
+         } else if (random === drawSquareRed) {
+             current = 40;
+
+
+         }
+         return random();
+     }
+
+
+     //Prevent window from moving up and down on keypress
+     window.addEventListener("keypress", function(event) {
+         event.preventDefault();
+     });
+
+     var ar = [33, 34, 35, 36, 37, 38, 39, 40];
+
+     $(document).keydown(function(e) {
+         var key = e.which;
+         //console.log(key);
+         //if(key==35 || key == 36 || key == 37 || key == 39)
+         if ($.inArray(key, ar) > -1) {
+             e.preventDefault();
+             return false;
+         }
+         return true;
+     });
+
+
+
+
+
+     var scoreId = document.getElementById('score-val');
+     //****************************CHECK IF KEYPRESS CORRESPONDS WITH CURRENT**************************??
+     function addKeyUp() {
+         window.addEventListener("keyup", function(event) {
+             event.preventDefault();
+             if (event.keyCode !== 32) {
+                 var timer = document.getElementById('time-remaining');
+                 if (timer.innerHTML != 0) {
+                     if (event.keyCode === current) {
+                         scoreId.innerHTML = score += 1;
+                         pickRandomShape()
+                         clearInterval(shapeTimer);
+                         shapeTimer = setInterval(function() {
+                             pickRandomShape();
+                         }, 1500);
+
+                     } else if (event.keyCode !== current) {
+                         scoreId.innerHTML = score -= 1;
+                         clearInterval(shapeTimer);
+                         shapeTimer = setInterval(function() {
+                             pickRandomShape();
+                         }, 1500);
+                     }
+                 }
+             }
+         });
+     }
+
+
+
+
+
+     //**********************GAME COUNTDOWN TIMER ***************************//                  
+     var count = 30;
+
+     function countdown() {
+         stopGame();
+         var timer = document.getElementById('time-remaining');
+         if (count === 30) {
+             pickRandomShape();
+         }
+         count--;
+         timer.innerHTML = count;
+
+         if (count <= 0) {
+             timer.innerHTML = 0;
+             window.removeEventListener('keyup', addKeyUp, true);
+         }
+     }
+
+     function stopGame() {
+         if (count <= 0) {
+             $('#overlay').show();
+             clearInterval(shapeTimer);
+             clearInterval(gameTimer);
+             ctx.clearRect(0, 0, canvas.width, canvas.height);
+             document.getElementById('restart-button').style.display = "block";
+             updateLeaderBoard();
+
+         }
+     }
+
+
+
+     var gameTimer;
+     var shapeTimer;
+     //****************SET INTERVAL ************************************//
+     window.addEventListener('keyup', function(e) {
+         e.preventDefault();
+         if (event.keyCode === 32) {
+             addKeyUp();
+             $('#overlay').hide();
+             gameTimer = setInterval(countdown, 1000);
+             shapeTimer = setInterval(function() {
+                 pickRandomShape();
+             }, 1500);
+             document.getElementById('start-game').style.display = "none";
+         }
+     });
+
+
+
+
+     //*********************RELOAD GAME **************************************//
+     document.getElementById('restart-button').addEventListener('click', function() {
+         location.reload();
+     });
+
+
+     function updateLeaderBoard() {
+         firebase.auth().onAuthStateChanged(function(user) {
+                 userArray = [];
+                 if (user) {
+                     // User is signed in.
+
+                     var displayName = user.displayName;
+                     var email = user.email;
+                     var emailVerified = user.emailVerified;
+                     var photoURL = user.photoURL;
+                     var uid = user.uid;
+                     var providerData = user.providerData;
+                     console.log(displayName)
+
+
+
+
+                     //Check User Score and if Current Score is Greater then User Score in Database, User Score in Database = Current Score
+
+                     firebase.database().ref().child('users').child(uid).child('score').once('value', function(snapshot) {
+                         var userDbScore = snapshot.val();
+
+
+                         if (userDbScore < score + 1) {
+                             //updating db score
+                             firebase.database().ref().child('users').child(uid).update({
+                                 score: scoreId.innerHTML
+                             });
+                             $('ol').empty();
+
+                             //Loop through Users and pull user info into array to be displayed in html
+                             firebase.database().ref().child('users').once('value', function(snapshot) {
+                                 var exists = (snapshot.val() !== null);
+                                 var userDataObj = snapshot.val();
+
+                                 for (let key in userDataObj) {
+                                     var userData = userDataObj[key]
+                                     console.log(userData.score)
+                                     userArray.push({
+                                         name: userData.username,
+                                         highScore: userData.score
+                                     })
+                                 }
+
+                             }).then(function() {
+                                 console.log(userArray);
+                                 var descending = userArray.sort((a, b) => Number(b.highScore) - Number(a.highScore));
+                                 console.log(descending);
+                                 var $oList = document.getElementById('leaderboard-ol');
+                                 var htmlStr = ""
+                                 descending.forEach(function(item) {
+                                     htmlStr += `<li>${item.name}: ${item.highScore}`
+                                 })
+                                 $('ol').append(htmlStr);
+                             });
+
+                         }
+                     });
+
+                 } else {
+                     // User is signed out.
+                 }
+             },
+             function(error) {
+                 console.log(error);
+             });
+
+     }
+
+
+
+ });
---
+++ shapes_game/leaderboard.js
@@ -0,0 +1,146 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDOmNu09H_PtwIDU5GOKtyoCTR2jqEEi4s",
+    authDomain: "shape-game-f01e9.firebaseapp.com",
+    databaseURL: "https://shape-game-f01e9.firebaseio.com",
+    storageBucket: "shape-game-f01e9.appspot.com",
+    messagingSenderId: "987459077457"
+};
+firebase.initializeApp(config);
+
+
+
+
+
+var uiConfig = {
+    signInSuccessUrl: '/index.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+
+//Loop through Users and pull user info into array to be displayed in html
+firebase.database().ref().child('users').once('value', function(snapshot) {
+    var exists = (snapshot.val() !== null);
+    var userDataObj = snapshot.val();
+
+    for (let key in userDataObj) {
+        var userData = userDataObj[key]
+        console.log(userData.score)
+        userArray.push({
+            name: userData.username,
+            highScore: userData.score
+        })
+    }
+
+}).then(function() {
+    console.log(userArray);
+    var descending = userArray.sort((a, b) => Number(b.highScore) - Number(a.highScore));
+    console.log(descending);
+    var $oList = document.getElementById('leaderboard-ol');
+    var htmlStr = ""
+    descending.forEach(function(item) {
+        htmlStr += `<li>${item.name}: ${item.highScore}`
+    })
+    $('ol').append(htmlStr);
+});
+
+
+
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+
+        if (user) {
+            // User is signed in.
+            //Hide Auth Button
+            $('#firebaseui-auth-container').hide();
+            $('#btnLogOut').show();
+
+
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            //if User is logged in show their name and profile-pic
+            $('#player').show();
+            $('#player-pic').attr('src', photoURL);
+            $('#player-name').text(displayName);
+
+            console.log(displayName)
+                //WRITE USER DATA WHEN SIGNED IN THE FIRST TIME
+            checkForFirstTime(uid);
+
+            function userFirstTimeCallback(userId, exists) {
+                if (exists) {
+                    console.log('user has logged in before!');
+                    // Do something here you want to do for non-firstime users...
+                } else {
+                    // Do something here you want to do for first time users (Store data in database?)
+                    console.log('user does not exist becuase they have never logged in before!');
+                    userArray.push({
+                        name: displayName,
+                        highScore: 0
+                    });
+                    console.log(userArray)
+
+                    function writeUserDataForTheFirstTime(userId, name, email) {
+                        firebase.database().ref('users/' + userId).set({
+                            username: name,
+                            email: email,
+                            score: 0
+
+                        });
+                    }
+                    writeUserDataForTheFirstTime(uid, displayName, email);
+                }
+            }
+
+            function checkForFirstTime(userId) {
+                firebase.database().ref().child('users').child(userId).once('value', function(snapshot) {
+                    var exists = (snapshot.val() !== null);
+                    userFirstTimeCallback(userId, exists);
+                });
+            }
+
+
+
+            //GET USER TOKEN
+            user.getToken().then(function(accessToken) {
+
+
+            });
+        } else {
+
+            //USER IS LOGGED OUT
+            console.log('user logged out')
+            $('#player').hide();
+
+        }
+    },
+    function(error) {
+        console.log(error);
+    });
+
+
+var logOut = document.getElementById("btnLogOut");
+
+logOut.addEventListener('click', e => {
+    firebase.auth().signOut();
+    $('#firebaseui-auth-container').show();
+    $('#btnLogOut').hide();
+    console.log("log out button hit");
+});
